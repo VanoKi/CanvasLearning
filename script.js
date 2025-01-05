e4bc1f5ac77e16f1d4946daf7f6dc30558f257e2
VanoKi <kirsvano@gmail.com>
2025-01-05 16:38:43 +0300

    feat: add two triangles

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ function draw() {
         const ctx = canvas.getContext('2d')
 
         ctx.beginPath();
-        ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Внешняя окружность
-        ctx.moveTo(110, 75);
-        ctx.arc(75, 75, 35, 0, Math.PI, false); // рот (по часовой стрелке)
-        ctx.moveTo(65, 65);
-        ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Левый глаз
-        ctx.moveTo(95, 65);
-        ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Правый глаз
+        ctx.moveTo(25, 25);
+        ctx.lineTo(105, 25);
+        ctx.lineTo(25, 105);
+        ctx.fill();
+
+        // Stroked triangle
+        ctx.beginPath();
+        ctx.moveTo(125, 125);
+        ctx.lineTo(125, 45);
+        ctx.lineTo(45, 125);
+        ctx.closePath();
         ctx.stroke();
     } else {
         console.log('unsupported')
