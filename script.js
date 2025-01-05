b7a6b30839f01afa8cf543da53424dd8e03558a5
VanoKi <kirsvano@gmail.com>
2025-01-05 16:32:48 +0300

    feat: add three rectangle

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function draw() {
     const canvas = document.querySelector('#tutorial')
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d')
-        ctx.fillStyle = 'rgb(200, 0, 0)'
-        ctx.fillRect(10, 10, 55, 50)
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
-        ctx.fillRect(30,30, 55, 50)
+
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
     } else {
         console.log('unsupported')
     }
