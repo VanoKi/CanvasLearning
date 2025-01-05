26546321b4312e8295193968a6864050b52c0bc9
VanoKi <kirsvano@gmail.com>
2025-01-05 16:30:26 +0300

    faet: add two rectangle

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function draw() {
     const canvas = document.querySelector('#tutorial')
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d')
+        ctx.fillStyle = 'rgb(200, 0, 0)'
+        ctx.fillRect(10, 10, 55, 50)
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
+        ctx.fillRect(30,30, 55, 50)
     } else {
         console.log('unsupported')
     }
