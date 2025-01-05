627b69acdb7b68a2090790eaa22297034fa4fa31
VanoKi <kirsvano@gmail.com>
2025-01-05 16:37:54 +0300

    feat: add smile

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ function draw() {
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d')
 
-        ctx.fillRect(25, 25, 100, 100);
-        ctx.clearRect(45, 45, 60, 60);
-        ctx.strokeRect(50, 50, 50, 50);
+        ctx.beginPath();
+        ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Внешняя окружность
+        ctx.moveTo(110, 75);
+        ctx.arc(75, 75, 35, 0, Math.PI, false); // рот (по часовой стрелке)
+        ctx.moveTo(65, 65);
+        ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Левый глаз
+        ctx.moveTo(95, 65);
+        ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Правый глаз
+        ctx.stroke();
     } else {
         console.log('unsupported')
     }
