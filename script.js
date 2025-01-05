84ebef6969fa3bca0c1bd6e1ed3c17ca277d67ec
VanoKi <kirsvano@gmail.com>
2025-01-05 16:25:14 +0300

    feat: add if

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const canvas = document.querySelector('#tutorial')
-const ctx = canvas.getContext('2d')
+function draw() {
+    const canvas = document.querySelector('#tutorial')
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d')
+    } else {
+        console.log('unsupported')
+    }
+}
