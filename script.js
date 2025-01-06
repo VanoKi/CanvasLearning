68910b28854ae697aef07f5cd7602a515361ba61
VanoKi <kirsvano@gmail.com>
2025-01-06 12:21:46 +0300

    feat: add arc

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
+// function draw() {
+//     const canvas = document.querySelector('#tutorial')
+//     if (canvas.getContext) {
+//         const ctx = canvas.getContext('2d')
+//
+//         ctx.beginPath()
+//
+//     } else {
+//         console.log('unsupported')
+//     }
+// }
 function draw() {
-    const canvas = document.querySelector('#tutorial')
+    var canvas = document.querySelector("#tutorial");
     if (canvas.getContext) {
-        const ctx = canvas.getContext('2d')
+        var ctx = canvas.getContext("2d");
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 3; j++) {
+                ctx.beginPath();
+                var x = 25 + j * 50; // x coordinate
+                var y = 25 + i * 50; // y coordinate
+                var radius = 20; // Arc radius
+                var startAngle = 0; // Starting point on circle
+                var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+                var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
 
-        ctx.beginPath();
-        ctx.moveTo(25, 25);
-        ctx.lineTo(105, 25);
-        ctx.lineTo(25, 105);
-        ctx.fill();
+                ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
 
-        // Stroked triangle
-        ctx.beginPath();
-        ctx.moveTo(125, 125);
-        ctx.lineTo(125, 45);
-        ctx.lineTo(45, 125);
-        ctx.closePath();
-        ctx.stroke();
-    } else {
-        console.log('unsupported')
+                if (i > 1) {
+                    ctx.fill();
+                } else {
+                    ctx.stroke();
+                }
+            }
+        }
     }
 }
