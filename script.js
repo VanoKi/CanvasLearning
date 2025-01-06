211f34b1d23f700d5b34265915664857d0e4aaba
VanoKi <kirsvano@gmail.com>
2025-01-06 17:32:41 +0300

    faet: add stars

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function draw() {
-    const ctx = document.querySelector("#tutorial").getContext("2d");
-    var lineJoin = ["round", "bevel", "miter"];
-    ctx.lineWidth = 10;
-    for (var i = 0; i < lineJoin.length; i++) {
-        ctx.lineJoin = lineJoin[i];
-        ctx.beginPath();
-        ctx.moveTo(-5, 5 + i * 40);
-        ctx.lineTo(35, 45 + i * 40);
-        ctx.lineTo(75, 5 + i * 40);
-        ctx.lineTo(115, 45 + i * 40);
-        ctx.lineTo(155, 5 + i * 40);
-        ctx.stroke();
+    const canvas = document.querySelector("#tutorial");
+    const ctx = canvas.getContext("2d");
+    // Clear canvas
+    ctx.fillStyle = '#001d3d'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    function drawStar(x, y, size) {
+        ctx.beginPath()
+        ctx.arc(x, y, size, 0, Math.PI * 2)
+        ctx.fillStyle = '#fff'
+        ctx.fill()
+        ctx.closePath()
+    }
+
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * canvas.width
+        const y = Math.random() * canvas.height
+        const size = Math.random() * 2 + 1
+        drawStar(x, y, size)
     }
 }
