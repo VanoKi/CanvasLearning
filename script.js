50e44d49af4b3cf3f8b686a74e50d766740948cd
VanoKi <kirsvano@gmail.com>
2025-01-06 15:53:11 +0300

    feat: drow lines

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 function draw() {
-    var ctx = document.querySelector("#tutorial").getContext("2d");
-    // Нарисовать фон
-    ctx.fillStyle = "rgb(255,221,0)";
-    ctx.fillRect(0, 0, 150, 37.5);
-    ctx.fillStyle = "rgb(102,204,0)";
-    ctx.fillRect(0, 37.5, 150, 37.5);
-    ctx.fillStyle = "rgb(0,153,255)";
-    ctx.fillRect(0, 75, 150, 37.5);
-    ctx.fillStyle = "rgb(255,51,0)";
-    ctx.fillRect(0, 112.5, 150, 37.5);
-
-    // Нарисовать полупрозрачные прямоугольники
-    for (var i = 0; i < 10; i++) {
-        ctx.fillStyle = "rgba(255,255,255," + (i + 1) / 10 + ")";
-        for (var j = 0; j < 4; j++) {
-            ctx.fillRect(5 + i * 14, 5 + j * 37.5, 14, 27.5);
-        }
+    const ctx = document.querySelector("#tutorial").getContext("2d");
+    for (let i = 0; i < 10; i++) {
+        ctx.lineWidth = 1 + i
+        ctx.beginPath()
+        ctx.moveTo(5 + i * 14, 5)
+        ctx.lineTo(5 + i * 14, 140)
+        ctx.stroke()
     }
 }
