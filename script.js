f4301ae82d67ce5b8eeffad591fd2c05099985db
VanoKi <kirsvano@gmail.com>
2025-01-06 12:23:44 +0300

    feat: add use quadratic

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-// function draw() {
-//     const canvas = document.querySelector('#tutorial')
-//     if (canvas.getContext) {
-//         const ctx = canvas.getContext('2d')
-//
-//         ctx.beginPath()
-//
-//     } else {
-//         console.log('unsupported')
-//     }
-// }
 function draw() {
-    var canvas = document.querySelector("#tutorial");
+    const canvas = document.querySelector('#tutorial')
     if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < 3; j++) {
-                ctx.beginPath();
-                var x = 25 + j * 50; // x coordinate
-                var y = 25 + i * 50; // y coordinate
-                var radius = 20; // Arc radius
-                var startAngle = 0; // Starting point on circle
-                var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-                var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
-
-                ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-
-                if (i > 1) {
-                    ctx.fill();
-                } else {
-                    ctx.stroke();
-                }
-            }
-        }
+        const ctx = canvas.getContext('2d')
+        // Quadratric curves example
+        ctx.beginPath();
+        ctx.moveTo(75, 25);
+        ctx.quadraticCurveTo(25, 25, 25, 62.5);
+        ctx.quadraticCurveTo(25, 100, 50, 100);
+        ctx.quadraticCurveTo(50, 120, 30, 125);
+        ctx.quadraticCurveTo(60, 120, 65, 100);
+        ctx.quadraticCurveTo(125, 100, 125, 62.5);
+        ctx.quadraticCurveTo(125, 25, 75, 25);
+        ctx.stroke();
+    } else {
+        console.log('unsupported')
     }
 }
